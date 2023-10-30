c742b2d9de0cb9dd328e71edfee8e56ec607c6cd
Jonathan Hernandez <jonathan.hdez92@gmail.com>
2023-10-29 21:29:17 -0400

    Send a message to local queue when posting a message to my dotnet api

--- Dotnet-Api/Controllers/WeatherForecastController.cs
+++ Dotnet-Api/Controllers/WeatherForecastController.cs
@@ -1,3 +1,7 @@
+using Amazon;
+using Amazon.Runtime;
+using Amazon.SQS;
+using Amazon.SQS.Model;
 using Microsoft.AspNetCore.Mvc;
 
 namespace quick_api.Controllers;
@@ -22,11 +26,41 @@ public class WeatherForecastController : ControllerBase
     public IEnumerable<WeatherForecast> Get()
     {
         return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> PostMessageAsync([FromBody] string messageContent)
+    {
+        try
         {
-            Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            TemperatureC = Random.Shared.Next(-20, 55),
-            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-        })
-        .ToArray();
+            // Set up the Amazon SQS client
+            var sqsConfig = new AmazonSQSConfig
+            {
+                ServiceURL = "http://localhost:4566", // Replace with your localstack endpoint
+            };
+            using var sqsClient = new AmazonSQSClient(new BasicAWSCredentials("ignore", "ignore"), sqsConfig);
+            // Define the message attributes
+            var messageRequest = new SendMessageRequest
+            {
+                QueueUrl = "http://localhost:4566/000000000000/my-quick-queue",
+                MessageBody = messageContent
+            };
+            // Send the message to the SQS queue asynchronously
+            var sendMessageResponse = await sqsClient.SendMessageAsync(messageRequest);
+            Console.WriteLine($"Message sent with message ID: {sendMessageResponse.MessageId}");
+            return Ok("Message sent successfully.");
+        }
+        catch (Exception ex)
+        {
+            // Handle exceptions, log errors, and return an appropriate response
+            Console.Error.WriteLine($"Error sending message: {ex.Message}");
+            return StatusCode(500, "Error sending the message to SQS.");
+        }
     }
 }
