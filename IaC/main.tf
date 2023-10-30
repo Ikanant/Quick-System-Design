9657ee06849330e9343a398c0c74902ac6b4eff7
Jonathan Hernandez <jonathan.hdez92@gmail.com>
2023-10-29 21:51:18 -0400

    Add a source mapping so my lambda function handles sqs messages
    Recall that the terraform creating the lambda and pushing it to localstack lives in a separate directory. Reference quick-lambda directory.

--- IaC/main.tf
+++ IaC/main.tf
@@ -5,3 +5,8 @@ resource "aws_s3_bucket" "bucket" {
 resource "aws_sqs_queue" "queue" {
   name = "my-quick-queue"
 }
+
+resource "aws_lambda_event_source_mapping" "sqs" {
+  event_source_arn = aws_sqs_queue.queue.arn
+  function_name    = "arn:aws:lambda:us-east-1:000000000000:function:QuickLambda"
+}
