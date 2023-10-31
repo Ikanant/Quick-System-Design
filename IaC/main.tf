823f3e802babcaf830d594fa6a022984dd1f635c
Jonathan Hernandez <jonathan.hdez92@gmail.com>
2023-10-30 23:35:02 -0400

    Create Notification Service topic and make SQS subscribe to it

--- IaC/main.tf
+++ IaC/main.tf
@@ -10,3 +10,13 @@ resource "aws_lambda_event_source_mapping" "sqs" {
   event_source_arn = aws_sqs_queue.queue.arn
   function_name    = "arn:aws:lambda:us-east-1:000000000000:function:QuickLambda"
 }
+
+resource "aws_sns_topic" "topic" {
+  name = "my-quick-topic"
+}
+
+resource "aws_sns_topic_subscription" "queue_sub" {
+  topic_arn = aws_sns_topic.topic.arn
+  protocol  = "sqs"
+  endpoint  = aws_sqs_queue.queue.arn
+}
